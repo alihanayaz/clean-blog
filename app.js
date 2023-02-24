962322d3666eb483f816b80ac597d291dffb758e
Alihan <alihanayaz0@gmail.com>
2023-02-24 13:45:42 +0300

    use template engine

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+
+app.get('/add-post', (req, res) => {
+  res.render('add_post');
 });
 
 app.listen(port, () => {

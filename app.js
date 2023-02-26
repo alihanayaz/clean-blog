ade03604b42e441f0be0adf365aed8e19c507a16
Alihan <alihanayaz0@gmail.com>
2023-02-26 23:17:49 +0300

    connect database

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post');
 const app = express();
 const port = 3000;
 
+mongoose.connect('mongodb://localhost/cleanblog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts: posts,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -20,6 +32,11 @@ app.get('/post', (req, res) => {
 
 app.get('/add-post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 app.listen(port, () => {
